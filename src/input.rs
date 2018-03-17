50fce0f9d0d1823fa7a4b9ee0875192f65516c90
David Lurton <psydave@gmail.com>
2018-03-17 14:05:36 -0700

    Add ability to lex integers, split into modules

---
+++ src/input.rs
@@ -0,0 +1,148 @@
+
+use std::str::Chars;
+use std::fmt;
+use std::collections::VecDeque;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Location {
+    /// The line of the location, starting at 1.
+    line: u32,
+    /// The byte offset from the start of the line, starting at 1.
+    col_offset: u32
+}
+
+impl Location {
+    pub fn start() -> Location {
+        Location { line: 1, col_offset: 1,  }
+    }
+
+    pub fn new(line: u32, col_offset: u32) -> Location {
+        Location { line, col_offset }
+    }
+
+    pub fn next(&self) -> Location {
+        Location { line: self.line, col_offset: self.col_offset + 1 }
+    }
+
+    pub fn next_line(&self) -> Location {
+        Location { line: self.line + 1, col_offset: 0 }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Span {
+    start: Location,
+    end: Location
+}
+
+impl Span {
+    pub fn new(start: Location, end: Location) -> Span {
+        Span { start, end }
+    }
+}
+
+
+pub struct CharsReader<'a> {
+    is_at_eof: bool,
+    input_chars: Chars<'a>,
+    lookahead: VecDeque<char>,
+    loc: Option<Location>
+}
+
+impl <'a> CharsReader<'a> {
+    pub fn new(input_chars: Chars<'a>) -> CharsReader<'a> {
+        CharsReader { is_at_eof: false, input_chars, lookahead: VecDeque::new(), loc: None }
+    }
+
+    pub fn loc(&self) -> Location {
+        match self.loc {
+            None => panic!("Must call .next() before taking the current location."),
+            Some(ref l) => l.clone() 
+        }
+    }
+
+    pub fn next(&mut self) -> Option<char> {
+        //Note: prime() will do nothing if end of stream has been previously reached.
+        self.prime(1);
+
+        match self.lookahead.pop_front() {
+            None => None,
+            Some(c) => {
+                self.loc = Some(match self.loc {
+                    None => Location::start(),
+                    Some(ref l) => if c == '\n' { l.next_line() } else { l.next() }
+                });
+                Some(c)
+            }
+        }
+    }
+
+    pub fn has_more(&self) -> bool {
+        !self.is_at_eof
+    }
+
+    pub fn peek(&mut self) -> Option<char> {
+        self.peek_n(0)
+    }
+
+    pub fn peek_n(&mut self, n: u32) -> Option<char> {
+        self.prime(n + 1);
+        if n as usize >= self.lookahead.len() { None } else { Some(self.lookahead[n as usize].clone()) }
+    }
+
+    fn prime(&mut self, min_chars: u32) {
+        while self.lookahead.len() < min_chars as usize && !self.is_at_eof {
+            match self.input_chars.next() {
+                Some(c) => self.lookahead.push_back(c),
+                None => self.is_at_eof = true
+            }
+        };
+    }
+}
+
+impl <'a> fmt::Debug for CharsReader <'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Reader {{ loc: {:?} }}", self.loc)
+    }
+}
+
+#[cfg(test)]
+mod reader_tests {
+    use super::*;
+    #[test]
+    fn reader_test() {
+
+        let src = "ab\ncd";
+        let mut reader = CharsReader::new(src.chars());
+
+        assert_eq!('a', reader.peek_n(0).unwrap());
+        assert_eq!('b', reader.peek_n(1).unwrap());
+        assert_eq!('\n', reader.peek_n(2).unwrap());
+        assert_eq!('c', reader.peek_n(3).unwrap());
+        assert_eq!('d', reader.peek_n(4).unwrap());
+        assert!(reader.peek_n(5).is_none());
+
+
+        let chr = reader.next().unwrap();
+        assert_eq!('a', chr);
+        assert_eq!(Location::new(1, 1), reader.loc());
+
+        let chr = reader.next().unwrap();
+        assert_eq!('b', chr);
+        assert_eq!(Location::new(1, 2), reader.loc());
+
+        let chr = reader.next().unwrap();
+        assert_eq!('\n', chr);
+        assert_eq!(Location::new(2, 0), reader.loc());
+
+        let chr = reader.next().unwrap();
+        assert_eq!('c', chr);
+        assert_eq!(Location::new(2, 1), reader.loc());
+
+        let chr = reader.next().unwrap();
+        assert_eq!('d', chr);
+        assert_eq!(Location::new(2, 2), reader.loc());
+
+        assert!(reader.next().is_none());
+    }
+}
---
+++ src/lexer.rs
@@ -0,0 +1,112 @@
+
+use std::str::Chars;
+
+use input::CharsReader;
+use input::Span;
+use input::Location;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenKind {
+    LiteralInt32(i32)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token {
+    kind: TokenKind,
+    span: Span
+}
+
+impl Token {
+    pub fn new(kind: TokenKind, span: Span) -> Token {
+        Token { kind, span }
+    }
+}
+
+fn is_white(chr: char) -> bool {
+    match chr {
+        ' ' | '\t' | '\n' | '\r' => true,
+        _ => false
+    }
+}
+
+fn is_digit(chr: char) -> bool {
+    chr >= '0' && chr <= '9'
+}
+
+pub struct Lexer<'a> {
+    reader: CharsReader<'a>
+}
+
+impl <'a> Lexer<'a> {
+    pub fn new(chars: Chars) -> Lexer {
+        return Lexer { reader: CharsReader::new(chars) }
+    }
+
+    pub fn next(&mut self) -> Option<Token> {
+        if self.reader.has_more() {
+            self.eat_white();
+            if let Some(token) = self.read_literal_number() {
+                Some(token)
+            } else {
+                panic!("Invalid character!")
+            }
+        } else {
+            None
+        }
+    }
+
+    fn eat_white(&mut self) {
+        while let Some(c) = self.reader.peek() {
+            if !is_white(c) {
+                break;
+            }
+            self.reader.next();
+        }
+    }
+
+    fn read_literal_number(&mut self) -> Option<Token> {
+        match self.reader.peek() {
+            None => None,
+            Some(c) => {
+                if !is_digit(c) {
+                    None
+                } else {
+                    let mut buf = String::new();
+                    buf.push(c);
+                    self.reader.next();
+                    let start = self.reader.loc();
+                    while let Some(c) = self.reader.peek() {
+                        if !is_digit(c) {
+                            break;
+                        }
+                        buf.push(c);
+
+                        self.reader.next();
+                    }
+
+                    // For now we don't care too much about error handling.
+                    let maybe_int = buf.parse::<i32>();
+                    match maybe_int {
+                        Ok(i) => Some(Token::new(TokenKind::LiteralInt32(i), Span::new(start, self.reader.loc()))),
+                        Err(_e) => panic!("Crap, it wasn't an integer!")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn lexer_test() {
+        let mut l = Lexer::new("  123  \n 456".chars());
+
+        assert_eq!(Token::new(TokenKind::LiteralInt32(123), Span::new(Location::new(1, 3), Location::new(1, 5))), l.next().unwrap());
+        assert_eq!(Token::new(TokenKind::LiteralInt32(456), Span::new(Location::new(2, 2), Location::new(2, 4))), l.next().unwrap());
+    }
+}
+
+
+  
